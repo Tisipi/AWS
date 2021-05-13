5e3d8295b6229e7fd0edea070a3fbee6c36112b9
Tisipi <tisipi@tisipi.nl>
2021-05-13 20:24:07 +0200

    Add list bucket contents

---
+++ bucket_list_objects.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import boto3
+
+
+MY_BUCKET = 'tisipi-com'
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(MY_BUCKET)
+for obj in bucket.objects.all():
+    print(obj.key)
--- bucket_upload_file.py
+++ bucket_upload_file.py
@@ -11,6 +11,6 @@ print('Upload a file to your S3 bucket')
 with open(MY_FILE, 'rb') as file_data:
     s3 = boto3.resource('s3')
     try:
-        s3.Bucket(MY_BUCKET).put_object(Key='MY_FILE', Body=file_data)
+        s3.Bucket(MY_BUCKET).put_object(Key=MY_FILE, Body=file_data)
     except:
         print('Error. Bucket does not exist')
